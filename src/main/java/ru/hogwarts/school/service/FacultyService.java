7a7940b7fbe5e50b9004d3fd558a8b27114f358c
semenikhin <garry@nextmail.ru>
2023-06-20 01:09:14 +0300

    First commit

---
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -0,0 +1,38 @@
+package ru.hogwarts.school.service;
+
+import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.model.Student;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class FacultyService {
+    private final Map<Long, Faculty> faculties = new HashMap<>();
+    private long countId = 0;
+
+    public Faculty addFaculty(Faculty faculty) {
+        faculty.setId(++countId);
+        faculties.put(countId, faculty);
+        return faculty;
+    }
+    public Faculty getFaculty(long id) {
+        return faculties.get(id);
+    }
+
+    public Faculty editFaculty(Faculty faculty) {
+        faculties.put(faculty.getId(), faculty);
+        return faculty;
+    }
+
+    public Faculty removeFaculty(long id) {
+        return faculties.remove(id);
+    }
+
+    public Collection<Faculty> filterByColor(String color) {
+        return faculties.values().stream()
+                .filter(e -> e.getColor().equals(color))
+                .collect(Collectors.toList());
+    }
+}
